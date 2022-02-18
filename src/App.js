c6ed1f5eb9ee619fe417b406b6a2cdbdf089f1d7
Your Name <you@example.com>
2022-02-19 00:22:47 +0100

    Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { myList } from './previous';
+
 
 function App() {
+  const h1 = <h1>Hello world!Omer</h1>;
+  const myArticle = <article>Omer Article</article>;
+  const p1 = <p id="large">foo</p>
+  const p2 = <p id="small">bar</p>
+  const myDiv = <div><h3>Hello world</h3></div>
+  const blog = (
+    <div>
+      <img src="omer.jpg" alt=""/>
+      <h3>
+        Welcome to Dan's Blog!
+      </h3>
+      <article>
+        Wow I had the tastiest sandwich today.  I <strong>literally</strong> almost freaked out.
+      </article>
+    </div>
+  );
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {h1}
+        <h2>JSX Elements</h2>
+        <p>Goodbye, Earth!</p>
+        <h2>JSX Elements And Their Surroundings</h2>
+        {myArticle}
+        <h2>Attributes In JSX</h2>
+        {p1}{p2}
+        <h2>Nested JSX</h2>
+        {myDiv}
+        <h2>JSX Outer Elements</h2>
+        {blog}
+        <h2>Rendering JSX</h2>
+        {myList}
       </header>
     </div>
+    
   );
 }
 
